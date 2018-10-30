f61d4f6005b85e20896c163e06113631dfd1d791
Angel Chou <cat77120377@hotmail.com>
2018-10-30 17:55:09 +0800

    version 1: switch black and white background

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
+    }
+    
+    func updateUI() {
+        view.backgroundColor = lightOn ? UIColor.white : UIColor.black
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    
+    
+    
 }
 
